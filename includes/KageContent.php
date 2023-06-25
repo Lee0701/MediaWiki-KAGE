17414540150a927526ddad92687dce317fa92389
Lee0701 <hgl1002@naver.com>
2023-06-25 17:43:59 +0900

    Put classname directly to the svg element

--- includes/KageContent.php
+++ includes/KageContent.php
@@ -3,7 +3,8 @@
 class KageContent {
 
     public static function format($input, $output) {
-        return "<span class='kage-result' alt='$input'>$output</span>";
+        // return "<span class='kage-result' alt='$input'>$output</span>";
+        return $output;
     }
 
 }
